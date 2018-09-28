68dadee7f299a833f0614b8e314c65f14ab2c94e
lautarodragan <lautaro.dragan@gmail.com>
2018-09-28 05:08:10 -0300

    everything

---
+++ a.js
@@ -0,0 +1,7 @@
+const kidA = require('kida')
+const amnesiac = require('amnesiac')
+const thekingoflimbs = require('thekingoflimbs')
+
+console.log('Kid A', kidA)
+console.log('Amnesiac', amnesiac)
+console.log('The King of Limbs', thekingoflimbs)
---
+++ b.js
@@ -0,0 +1,11 @@
+const kidA = require('kida')
+
+console.log('Kid A', kidA)
+
+const amnesiac = require('amnesiac')
+
+console.log('Amnesiac', amnesiac)
+
+const thekingoflimbs = require('thekingoflimbs')
+
+console.log('The King of Limbs', thekingoflimbs)
